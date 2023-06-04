ac9f8001f03672b0b856311877655505bea478b8
ingarbi <104501854+ingarbi@users.noreply.github.com>
2023-06-04 12:38:58 +0300

    #2 layout

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,8 @@
+const Footer = () => {
+  return <footer>
+    <div>2023 MyApp. All rights reserved</div>
+    <div></div>
+  </footer>;
+};
+
+export default Footer;
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,49 @@
+import Link from "next/link"
+
+const links = [
+    {
+      id: 1,
+      title: "Home",
+      url: "/",
+    },
+    {
+      id: 2,
+      title: "Portfolio",
+      url: "/portfolio",
+    },
+    {
+      id: 3,
+      title: "Blog",
+      url: "/blog",
+    },
+    {
+      id: 4,
+      title: "About",
+      url: "/about",
+    },
+    {
+      id: 5,
+      title: "Contact",
+      url: "/contact",
+    },
+    {
+      id: 6,
+      title: "Dashboard",
+      url: "/dashboard",
+    },
+  ];
+
+const Navbar = () => {
+  return (
+    <nav>
+        <Link href='/'>MyAPP</Link>
+        <div>
+            {links.map((link) =>(
+                <Link key={link.id} href={link.url}>{link.title}</Link>
+            ))}
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
