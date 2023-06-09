b97e12b3c9c5c84c55b634afdbe8024de176f57a
ingarbi <104501854+ingarbi@users.noreply.github.com>
2023-06-09 15:47:14 +0300

    #10 register

--- src/models/User.js
+++ src/models/User.js
@@ -22,4 +22,5 @@ const userSchema = new Schema(
   { timestamps: true }
 );
 
-export default mongoose.model("User", userSchema);
+// export default mongoose.model("User", userSchema);
+export default mongoose.models.User || mongoose.model("User", userSchema);
